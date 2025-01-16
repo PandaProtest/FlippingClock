1f587c25aedf5c8bed78abaf6a57fce360062431
Panda Protest <103907040+PandaProtest@users.noreply.github.com>
2025-01-16 14:32:08 -0800

    Create __init__.py

---
+++ flipping_clock/__init__.py
@@ -0,0 +1,11 @@
+"""
+Flipping Clock - A full-screen clock with flipping animations for Linux.
+
+Author: pandaprotest
+Version: 1.0.0
+"""
+
+from .main import start_clock
+
+__version__ = "1.0.0"
+__author__ = "pandaprotest”
